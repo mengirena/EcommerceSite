7e919608df453baa686cced0df85ad8e622aef1d
Irena Chen <irena1009@gmail.com>
2021-07-11 13:48:50 -0700

    create a post route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
+
 const cors = require("cors")
 const express = require("express")
 //todo: add stripe key
-const stripe = require("stripe")("")
+const stripe = require("stripe")(process.env.SECRET_KEY)//paste secret key;
 const { v4: uuid } = require("uuid")
 
 const app = express()
@@ -13,6 +15,35 @@ app.use(cors()) //for cross object references
 //routes
 app.get("/", (req, res) => {
     res.send("works")
+})
+
+
+app.post("/payment", (req, res)=>{
+    const { product, token} = req.body
+    console.log("product", product)
+    console.log("price", product.price)
+    const idempotencyKey = uuid()
+
+    return stripe.customers.create({
+        email: token.email,
+        source: token.id
+    }).then(customer => {
+        stripe.charges.create({
+            amount: product.price * 100,
+            currency: 'usd',
+            customer: customer.id,
+            receipt_email: token.email,
+            description: `purchase of ${product.name}`,
+            shipping:{
+                name: token.card.name,
+                address: {
+                    country: token.card.address_country
+                }
+            }
+        },{idempotencyKey})
+    }).then(result => res.status(200).json(result))
+    .catch(err => console.log(err))
+
 })
 
 //listening
